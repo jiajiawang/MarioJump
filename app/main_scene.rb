308b6c8836f8937a30e7a9e0207ab0a77f31fefb
Jiajia Wang <jjwang0506@gmail.com>
2015-10-03 20:45:24 +1000

    Add ground

--- app/main_scene.rb
+++ app/main_scene.rb
@@ -1,6 +1,19 @@
 class MainScene < MG::Scene
   def initialize
+    self.gravity = [0, -900]
+
     add_mario
+    add_ground
+  end
+
+  def add_ground
+    1.upto(15) do |offset|
+      block = MG::Sprite.new('block.png')
+      block.attach_physics_box
+      block.dynamic = false
+      block.position = [200 + (offset-1) * 48, 100]
+      add block
+    end
   end
 
   def add_mario
